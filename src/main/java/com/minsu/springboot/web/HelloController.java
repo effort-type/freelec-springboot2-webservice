e8ef9c4f03f4d0d87227abfbed5124b7011024f2
minsu <jminsu084@gmail.com>
2022-01-06 15:47:11 +0900

    77Page

--- src/main/java/com/minsu/springboot/web/HelloController.java
+++ src/main/java/com/minsu/springboot/web/HelloController.java
@@ -1,13 +1,20 @@
 package com.minsu.springboot.web;
+import com.minsu.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController // JSON으로 반환하는 컨트롤러로 만들어준다.
 public class HelloController {
 
-    @GetMapping // HTTP Method인 Get의 요청을 받을 수 있는 API를 만들어 줍니다.
+    @GetMapping ("/hello")// HTTP Method인 Get의 요청을 받을 수 있는 API를 만들어 줍니다.
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloResponseDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
     }
 }
