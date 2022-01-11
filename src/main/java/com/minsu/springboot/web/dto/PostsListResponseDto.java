be7833e5428e8747cde5cfde34f75714f3c5714e
minsu <jminsu084@gmail.com>
2022-01-11 16:06:19 +0900

    게시글 전체 조회 화면 구현 완료 (~ page 150)

---
+++ src/main/java/com/minsu/springboot/web/dto/PostsListResponseDto.java
@@ -0,0 +1,24 @@
+package com.minsu.springboot.web.dto;
+
+
+import com.minsu.springboot.domain.posts.Posts;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+
+}
