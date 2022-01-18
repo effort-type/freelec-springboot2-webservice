aeb5bcfc25a92cb13640185e25e5ffe085203d5e
Minsu <jminsu084@gmail.com>
2022-01-18 17:43:40 +0900

    2장 오류 해결 및 정리

--- src/main/java/com/minsu/springboot/Application.java
+++ src/main/java/com/minsu/springboot/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@SpringBootApplication
-public class Application {
+@SpringBootApplication // 스프링 부트의 자동 설정, 스프링 Bean 읽기와 생성을 모두 자동으로 설정해줌
+public class Application { // 프로젝트의 메인 클래스
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args); // 내장 WAS(Web Application Server) 실행
     }
 }
--- src/test/java/com/minsu/springboot/web/HelloControllerTest.java
+++ src/test/java/com/minsu/springboot/web/HelloControllerTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -14,28 +16,31 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
-@WebMvcTest(controllers =  {HelloController.class}, secure = false)
+@RunWith(SpringRunner.class) // 스프링 부트 테스트와 JUnit 사이에 연결자 역할을 한다.
+@WebMvcTest(controllers =  {HelloController.class}, secure = false) // 스프링 테스트 어노테이션 중, Web에 집중할 수 있는 어노테이션
+@MockBean(JpaMetamodelMappingContext.class) // Test 안되는 오류 해결하기 위함
 public class HelloControllerTest {
 
-    @Autowired
-    private MockMvc mvc;
+    @Autowired // 스프링이 관리하는 Bean을 주입 받는 어노테이션
+    private MockMvc mvc; // 웹 API를 테스트 할 때 사용, 스프링 MVC 테스트의 시작점, 이 클래스를 통해 웹에 대한 API 테스트 가능
+
 
     @Test
     public void return_hello() throws Exception {
         String hello = "hello";
 
-        mvc.perform(get("/hello"))
-                .andExpect(status().isOk())
+        mvc.perform(get("/hello")) // ()안 주소로 HTTP GET 요청
+                .andExpect(status().isOk()) // HTTP Header의 Status를 검증 ex) 200, 404, 500 등 상태 점검, mvc.perform의 결과를 검증할 때 .andExpect사용
                 .andExpect(content().string(hello));
     }
 
+    @Test
     public void result_helloDto() throws Exception {
         String name = "hello";
         int amount = 1000;
         mvc.perform(get("/hello/dto").param("name", name).param("amount", String.valueOf(amount)))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.name", is(name))) // JSON 응답값을 필드별로 검증할 수 있는 메소드
                 .andExpect(jsonPath("$.amount", is(amount)));
 
     }
